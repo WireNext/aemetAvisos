f49b7ce68ee7b6a907159be859ec41e1ebac9b21
WireNext <wirennextt@gmail.com>
2025-03-03 13:26:38 +0100

    Update procesar_geojson.py

--- procesar_geojson.py
+++ procesar_geojson.py
@@ -234,7 +234,7 @@ def procesar_geojson():
 
                         geojson_combinado["features"].append(feature)
 
-print(f"GeoJSON combinado: {geojson_combinado}")  # Añadir esta línea
+    print(f"GeoJSON combinado: {geojson_combinado}")  # Añadir esta línea
 
     # Verificar si el archivo ya existe y eliminarlo
     if os.path.exists(SALIDA_GEOJSON):
